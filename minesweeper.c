a7f9e69eb965b9a4b58c00aa21b9f3af5268329e
marcelscpn <mschopen1997@gmail.com>
2020-10-11 17:27:49 +0100

    now finally working. still need to finish writing bot

--- minesweeper.c
+++ minesweeper.c
@@ -12,14 +12,20 @@ typedef struct board board;
 
 void print_layover(board* layover){
 	int i, j;
+	printf("  ");
+	for(i = 0; i < layover->width; i++){
+		printf("%d ", i % 10);
+	}
+	printf("\n");
 	for(i = 0; i < layover->height; i++){
+		printf("%d ", i % 10);
 		for(j = 0; j < layover->width; j++){
 			switch(*(*(layover->cell + i) + j)){
 				case -4: printf("f ");
 					 break;
 				case -3: printf("# ");
 					 break;
-				case -2: printf("x ");
+				case -2: printf("* ");
 					 break;
 				case -1: printf(". ");
 					 break;
@@ -68,7 +74,28 @@ board* create_new_board(int h, int w, int num){
 	return ret;
 }
 
-int neighboring_mines(board* mines, int x, int y){
+int count_neighboring_indices(board* layover, int x, int y, int ind){
+        int sum = 0;
+        if(x - 1 >= 0 && *(*(layover->cell + y) + x -1) == ind)
+                sum += *(*(layover->cell + y) + x - 1);
+        if(x - 1 >= 0 && y + 1 < layover->width && *(*(layover->cell + y + 1) + x -1) == ind)
+                sum += *(*(layover->cell + y + 1) + x - 1);
+        if(y + 1 < layover->width && *(*(layover->cell + y + 1) + x) == ind)
+                sum += *(*(layover->cell + y + 1) + x);
+        if(x + 1 < layover->height && y + 1 < layover->width && *(*(layover->cell + y + 1) + x + 1) == ind)
+                sum += *(*(layover->cell + y + 1) + x + 1);
+        if(x + 1 < layover->height && *(*(layover->cell + y) + x + 1) == ind)
+                sum += *(*(layover->cell + y) + x + 1);
+        if(x + 1 < layover->height && y - 1 >= 0 && *(*(layover->cell + y - 1) + x) == ind)
+                sum += *(*(layover->cell + y - 1) + x + 1);
+        if(y - 1 >= 0 && *(*(layover->cell + y - 1) + x) == ind)
+                sum += *(*(layover->cell + y - 1) + x);
+        if(x - 1 >= 0 && y - 1 >= 0 && *(*(layover->cell + y - 1) + x - 1) == ind)
+                sum += *(*(layover->cell + y - 1) + x - 1);
+        return sum;
+}
+
+/*int neighboring_mines(board* mines, int x, int y){
 	int sum = 0;
 	if(x - 1 >= 0)
 		sum += *(*(mines->cell + y) + x - 1);
@@ -87,7 +114,7 @@ int neighboring_mines(board* mines, int x, int y){
 	if(x - 1 >= 0 && y - 1 >= 0)
 		sum += *(*(mines->cell + y - 1) + x - 1);
 	return sum;
-}
+}*/
 
 
 int flag(board* layover, int x, int y){
@@ -124,9 +151,9 @@ int uncover(board* layover, board *mines, int x, int y, int rec){
 		}
 		return -1;
 	}
-	int n;
+	int n;	
 	if(*(*(mines->cell + y) + x) == 0){	
-		n = neighboring_mines(mines, x, y);
+		n = count_neighboring_indices(mines, x, y, 1);
 		*(*(layover->cell + y) + x) = n;
 		if(n == 0){
 			if(x - 1 >= 0) 
@@ -150,18 +177,35 @@ int uncover(board* layover, board *mines, int x, int y, int rec){
 	}
 }
 
-int strategy(board* layover, board* mines, char *output, int step){
-	output = (char*)malloc(10*sizeof(char));
+int check_covered(board* layover){
+	int i, j;
+	for(i = 0; i < layover->height; i++){
+		for(j = 0; j < layover->width; j++){
+			if(*(*(layover->cell + i) + j) == -1){
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+/*int strategy(board* layover, board* mines, int step){
 	if(step == 0){
 		uncover(layover, mines, 0, 0, 0);
-		output = "u 0 0";
-		//printf("OUTPUT: %s", output);
+		printf("%d> u 0 0\n", step);
 		return 0;
 	}
 	else{
-		return 1;
+		int i, j, N, n;
+		for(i = 0; i < layover->height; i++){
+			for(j = 0; j < layover->width; j++){
+				if(*(*(layover->cell + j) + i) == 1 && count_neighboring_indices(j, i, -1) == 1){
+					
+				}	
+			}
+		}
 	}
-}
+}*/
 
 
 int main(int argc, char* argv[]){
@@ -183,7 +227,7 @@ int main(int argc, char* argv[]){
 		while(1){
 			print_layover(layover);
 			printf("%d> ", steps);
-			scanf(" %c %d %d", &c, &x, &y);
+			scanf(" %c %d %d", &c, &y, &x);
 			//printf("INPUT SCANNED: %c %d %d\n", c, x, y);
 			if(x < 0 || x >= w || y < 0 || y >= h){
 				printf("Coordinates not in range\n");
@@ -197,7 +241,7 @@ int main(int argc, char* argv[]){
 			}
 			else if(c == 'u'){
 				switch(uncover(layover, mines, x, y, 0)){
-					case 1: printf("Cannot uncover cells already uncovered\n");
+					case 1: printf("Cannot uncover cells already uncovered or flagged\n");
 						break;
 					case -1: printf("You clicked on a mine! Idiot...\n");
 						 print_layover(layover);
@@ -205,20 +249,23 @@ int main(int argc, char* argv[]){
 				}
 			}
 			steps++;
+			if(check_covered(layover)){
+				printf("Game finished, you've found all mines!\n");
+			       	print_layover(layover);	
+				return 0;
+			}
 		}
 	}
 	else{
-		char *output;
-		while(1){
+	/*	while(1){
 			print_layover(layover);
-			if(!strategy(layover, mines, output, steps))
-				printf("%d> %s", steps, output);
-			else{
-				printf("Computer has given up\n");
-				return 0;
+			if(strategy(layover, mines,  steps)){
+				printf("Computer has finished\n");
 			}
 			steps++;	
 		}
+	*/
+	printf("Strategy not yet implemented\n");
 	}
 	return 0;
 }
