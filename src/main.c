0c0378513d107883dcbaedb54527fb6ebd3c42cd
marcelscpn <mschopen1997@gmail.com>
2020-10-20 18:26:39 +0100

    ADD: template for getopt()API

--- src/main.c
+++ src/main.c
@@ -2,6 +2,44 @@
 #include "strategy.h"
 
 #include <stdio.h>
+#include <unistd.h>
+
+// int main (int argc, char *argv[]) {
+// int opt = 0;
+// char *in_fname = NULL;
+// char *out_fname = NULL;
+
+// while ((opt = getopt(argc, argv, "i:o:")) != -1) {
+//     switch(opt) {
+//     case 'i':
+//     in_fname = optarg;
+//     printf("\nInput option value=%s", in_fname);
+//     break;
+//     case 'o':
+//     out_fname = optarg;
+//     printf("\nOutput option value=%s", out_fname);
+//     break;
+//     case '?':
+//     /* Case when user enters the command as
+//      * $ ./cmd_exe -i
+//      */
+//     if (optopt == 'i') {
+//     printf("\nMissing mandatory input option");
+//     /* Case when user enters the command as
+//      * # ./cmd_exe -o
+//      */
+//   } else if (optopt == 'o') {
+//      printf("\nMissing mandatory output option");
+//   } else {
+//      printf("\nInvalid option received");
+//   }
+//   break;
+//  }
+//  }
+
+// printf("\n");
+// return 0;
+//  }
 
 int main(int argc, char* argv[]){
 	if(argc != 5){
