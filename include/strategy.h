71a8cecdde20e18ec5bd27e9a70930fc7e5c38a4
marcelscpn <mschopen1997@gmail.com>
2020-10-20 22:44:01 +0100

    [NOT FUNCTIONAL]: ADD strategies

--- include/strategy.h
+++ include/strategy.h
@@ -1,4 +1,8 @@
 #ifndef __STRATEGY_H__
 #define __STRATEGY_H__
 
+int s_first_click(board* layover, board* mines, int steps);
+int s_recursive(board* layover, board* mines, int steps);
+int s_recursive_random(board* layover, board* mines, int steps);
+
 #endif
--- src/strategy.c
+++ src/strategy.c
@@ -1,1 +1,25 @@
 #include "strategy.h"
+#include "board.h"
+
+#include <time.h>
+#include <stdlib.h> 
+
+int (*strategy_pointer)(board* layover, board* mines, int steps) = NULL;
+
+int s_first_click(board* layover, board* mines, int steps){
+	if(step == 0){
+		uncover(layover, mines, 0, 0, 0);
+		printf("%d> u 0 0\n", step);
+		return 0;
+	}
+	else{
+		int i, j, N, n;
+		for(i = 0; i < layover->height; i++){
+			for(j = 0; j < layover->width; j++){
+				if(*(*(layover->cell + j) + i) == 1 && count_neighboring_indices(j, i, -1) == 1){
+					
+				}	
+			}
+		}
+	}
+}
