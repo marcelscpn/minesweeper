70638f5b15cf217df277170f8d60db9b56e39b3e
marcelscpn <mschopen1997@gmail.com>
2020-10-21 10:58:09 +0100

    [NOT FUNCTIONAL YET] more work on testing routines

--- test/minunit.h
+++ test/minunit.h
@@ -1,4 +1,12 @@
+//#include <stdarg.h>
+
+#ifndef __MINUNIT_H__
+#define __MINUNIT_H__
+
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
 #define mu_run_test(test) do { char *message = test(); tests_run++; if (message) return message; } while (0)
 extern int tests_run;
 
+int assert_array_eq(int num, int size, int type...);
+
+#endif
