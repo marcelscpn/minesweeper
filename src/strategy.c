83d03b388b975a8fff1f9989aad0effe1eaf3f14
marcelscpn <mschopen1997@gmail.com>
2020-12-20 17:13:58 +0100

    MOD: simple search strategy now fixed. Works;

--- src/strategy.c
+++ src/strategy.c
@@ -13,6 +13,7 @@ strategy_ptr select_strategy(int mode){
         case 2:
             return s_simple_search;
     }
+    return NULL;
 }
 
 command* s_first_click(board* layover, int steps){
@@ -29,23 +30,21 @@ command* s_simple_search(board* layover, int steps){
         return uncover_command(0, 0);
     }
     int n_mines, n_flags;
-    int* n_empty_fields;
-    command* cmd;
+    int* n_covered;
     for(int i = 0; i < layover->height; i++){
         for(int j = 0; j < layover->width; j++){
             n_mines = *(*(layover->cell + i) + j);
             if(n_mines > 0){
-                printf("%d,%d, %d\n", i, j, n_mines);
-                n_empty_fields = index_neighborhood(layover, i, j, -1);
-                n_flags = count_neighboring_indices(layover, i, j, -2);
-                if(n_mines - n_flags == *n_empty_fields){
-                    int y = *(n_empty_fields + 1);
-                    int x = *(n_empty_fields + 2);
+                n_covered = index_neighborhood(layover, j, i, -1);
+                n_flags = count_neighboring_indices(layover, j, i, -2);
+                if(n_mines - n_flags == *n_covered && *n_covered > 0){
+                    int y = *(n_covered + 1);
+                    int x = *(n_covered + 2);
                     return flag_command(x, y);
                 }
-                if(n_mines - n_flags == 0 && *n_empty_fields > 0){
-                     int y = *(n_empty_fields + 1);
-                     int x = *(n_empty_fields + 2);
+                if(n_mines - n_flags == 0 && *n_covered > 0){
+                     int y = *(n_covered + 1);
+                     int x = *(n_covered + 2);
                      return uncover_command(x, y);
 
                 }
