e21e3d991de1ac4b71802fd31b3f565ba0150d8b
marcelscpn <mschopen1997@gmail.com>
2020-12-20 16:01:16 +0100

    DEL: unused old code; FIX: boards are now freed using free_board; command is fully initialized (the give_up flag is set) when in manual mode;

--- src/main.c
+++ src/main.c
@@ -17,8 +17,7 @@ int main(int argc, char* argv[]){
 	sscanf(argv[4], "%d", &num);
 	board* mines = create_new_board(h, w, num);
 	board* layover = create_layover(h, w, -1);
-	char c;
-	int x, y, steps = 0;
+    int steps = 0;
     command* cmd;
     strategy_ptr strategy;
     if(mode){
@@ -29,6 +28,7 @@ int main(int argc, char* argv[]){
         printf("%d> ", steps);
         if(!mode){
             cmd = (command*)malloc(sizeof(command));
+            cmd->give_up = 0;
             scanf(" %c %d %d", &cmd->c, &cmd->y, &cmd->x);
         }
         else{
@@ -51,82 +51,21 @@ int main(int argc, char* argv[]){
             case 1:
                 printf("Mine clicked. Dumb!\n");
                 print_layover(layover);
-                return 0;
+                goto RET;
             case 3:
                 printf("Computer has given up.\n");
                 print_layover(layover);
-                return 0;
+                goto RET;
         }
         if(check_covered(layover)){
             printf("Game finished, you've found all mines!\n");
             print_layover(layover);	
-            return 0;
+            goto RET;
         }
         steps++;
-   }
-//	if(!mode){
-//		while(1){
-//			print_layover(layover);
-//			printf("%d> ", steps);
-//			scanf(" %c %d %d", &c, &y, &x);
-//			//printf("INPUT SCANNED: %c %d %d\n", c, x, y);
-//			if(x < 0 || x >= w || y < 0 || y >= h){
-//				printf("Coordinates not in range\n");
-//				steps++;
-//				continue;	
-//			}
-//			if(c == 'f'){
-//				if(flag(layover, x, y)){
-//					printf("Cannot flag cells already uncovered\n");
-//				}
-//			}
-//			else if(c == 'u'){
-//				switch(uncover(layover, mines, x, y, 0)){
-//					case 1: printf("Cannot uncover cells already uncovered or flagged\n");
-//						break;
-//					case -1: printf("You clicked on a mine! Idiot...\n");
-//						 print_layover(layover);
-//						 return 0;
-//				}
-//			}
-//			steps++;
-//			if(check_covered(layover)){
-//				printf("Game finished, you've found all mines!\n");
-//			       	print_layover(layover);	
-//				return 0;
-//			}
-//		}
-//	}
-//	else{
-//        strategy_ptr strategy = select_strategy(mode);
-//        command* cmd; 
-//        while(1){
-//            print_layover(layover);
-//			printf("%d> ", steps);
-//            cmd = strategy(layover, steps);
-//            print_command(cmd);
-//            switch(parse_command(cmd)){
-//                case 0:
-//                    break;
-//                case -1:
-//                    printf("Cannot uncover cells already uncovered or flagged\n");
-//                    break;
-//                case -2:
-//                    
-//                case 1:
-//                    printf("Computer has clicked on a mine. Dumb Computer.\n");
-//                    print_layover(layover);
-//                    return 0;
-//                case 2:
-//                    printf("Game finished, computer has found all mines!\n");
-//                    print_layover(layover);
-//                    return 0;
-//                case 3:
-//                    printf("Computer has given up.\n");
-//                    print_layover(layover);
-//                    return 0;
-//            }
-//        }
-//    }
+    }
+    RET:
+    free_board(mines);
+    free_board(layover);
 	return 0;
 }
